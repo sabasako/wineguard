b5eb312b247ca3b9cad376250e6a9cdfcf0debda
Saba Sakandelidze <saba.saka01@gmail.com>
2023-12-17 00:32:29 +0400

    Fix formatting and add functionality to parts subheadings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,15 @@
-"use strict";
-
 const orderButton = document.querySelectorAll(".button-order");
 const modal = document.querySelector(".modal");
 const modalBackground = document.querySelector(".modal-background");
 const closeBtn = document.querySelector(".close-button");
 
+const partsSubheading = document.querySelectorAll(".parts-subheading");
+const content = document.querySelectorAll(".content");
+const dropdown = document.querySelector(".dropdown");
+const moreHeading = document.querySelector(".more-heading");
+
 function removeModal(e) {
-  if (e.key === "ESQ") {
+  if (e.key === "Escape") {
     document.body.classList.remove("modal-active");
     modal.classList.remove("show");
     modalBackground.classList.remove("background-show");
@@ -30,3 +33,34 @@ orderButton.forEach((btn) => {
 closeBtn.addEventListener("click", removeModal);
 modalBackground.addEventListener("click", removeModal);
 window.addEventListener("keydown", removeModal);
+
+partsSubheading.forEach((subheading) => {
+  subheading.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // subheading.nextElementSibling.classList.toggle("show");
+    subheading.nextElementSibling.classList.toggle("show");
+    subheading.querySelector(".drop-down-icon").classList.toggle("show");
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  // direction: "vertical",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
